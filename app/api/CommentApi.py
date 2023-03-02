256a66846747782393736be0d100483759da01fa
yuzai_win <yuzaii@qq.com>
2023-03-02 22:50:25 +0800

    去除一些注释

--- app/api/CommentApi.py
+++ app/api/CommentApi.py
@@ -40,7 +40,7 @@ def add_child_commmets(parent_commments, all_comments, comment_user_dict):
     for parent_commmet in parent_commments:
         # 定义一个孩子评论列表
         child_comments = []
-        print(parent_commmet)
+        # print(parent_commmet)
         # 遍历所有评论列表 筛选自己的孩子评论
         for child_comment in all_comments:
             if child_comment.parent_comment_id == parent_commmet.comment_id:
@@ -62,17 +62,16 @@ def getcommnets(post_id: int, db: Session = Depends(get_db)):
     # 先获取这个id的所有评论吧
     all_comments = db.query(Comment).filter_by(post_id=post_id).options(
         subqueryload(Comment.user).load_only(User.username, User.avatar)).all()
-    print("一开始从数据库中获取的列表", all_comments)
+    # print("一开始从数据库中获取的列表", all_comments)
     print("一个获取到", len(all_comments), "条评论")
     # 将评论变成字典key是comment_id value这条评论者的名字
     comment_user_dict = {obj.comment_id: obj.user for obj in all_comments}
-    print(comment_user_dict)
+    # print(comment_user_dict)
     # print(commentlist)
     # 先挑出主评论 下面已经排序
     parent_commments = generate_parent_commmets(all_comments)
     commentlist = add_child_commmets(parent_commments, all_comments, comment_user_dict)
-    print('----')
-    print(commentlist)
+    # print(commentlist)
 
     # print(parent_commmets)
     # print("逐个输出")
@@ -81,7 +80,6 @@ def getcommnets(post_id: int, db: Session = Depends(get_db)):
     # # print(sorted_commentlist)
     # # print(len(commentlist_resp))
     # print(all_comments)
-    # # TODO 记得排序
     return {'code': 200, 'msg': 'success', 'data': {'commentlist': commentlist}}
 
 
